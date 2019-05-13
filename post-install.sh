9fb37ea623845a9eb579f6975a2767c922c8142d
jri-sp <jeremy.richard@sciencespo.fr>
2019-05-13 16:37:39 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,7 +3,6 @@ sudo usermod -G docker debian
 sudo apt-get install -y curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-whoami
 git clone https://github.com/medialab/hyphe.git /opt/hyphe
 chown -R debian:debian /opt/hyphe
 cd /opt/hyphe
